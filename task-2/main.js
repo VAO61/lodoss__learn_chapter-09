2c37ef66cd1cce0ec8e0d6afdc178a70cc4ab86a
VAO61 <vao.frontend@gmail.com>
2019-05-17 19:36:39 +0300

    pre-alpha 0.0.1

--- task-2/main.js
+++ task-2/main.js
@@ -1,1 +1,24 @@
-// start project "task-2"
+function emailValidation() {}
+
+/*
+
+Недействительны:
+
+@gmail.ru
+xxx@yyy@gmail.ru
+.v2-111@gmail.ru
+_name@gmail.ru
+-name@gmail.ru
+vao@gmail
+vao..frontend@gmail.com
+vao frontend@gmail.com
+
+Действительны: 
+
+vao_front-END@gmail.com
+vao.frontend@gmail.com
+
+*/
+
+// \S+@\w+\.\w+
+// ([A-Za-z0-9])+@\w+\.\w+
