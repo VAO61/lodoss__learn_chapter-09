353ddf7d6e3570fa8b68106588d84f548631ad87
VAO61 <vao.frontend@gmail.com>
2019-05-17 16:20:57 +0300

    pre-alpha 0.0.1

--- task-1/main.js
+++ task-1/main.js
@@ -1,1 +1,22 @@
-function markdownToHTML() {}
+/* const markdownToHTMLInput = document.getElementById('markdownToHTMLInput');
+const markdownToHTMLResult = document.getElementById('markdownToHTMLResult');
+// const input = '**text**';
+function markdownToHTML() {
+  let text = markdownToHTMLInput.nodeValue;
+  // input.replace();
+  text = text.replace(/#.+/, '<h1>$&</h1>');
+  // text = text.replace(/<h1>#/, '<h1>');
+  // let result = input.replace(/\*{2}.+\*{2}/, '<b>$&</b>');
+  markdownToHTMLResult.innerHTML = text;
+}
+// markdownToHTML();
+console.log(markdownToHTML()); */
+
+const input = '**text**';
+function markdownToHTML() {
+  input.replace();
+  let result = input.replace(/\*{2}.+\*{2}/, '<b>');
+  return result;
+}
+// markdownToHTML();
+console.log(markdownToHTML());
