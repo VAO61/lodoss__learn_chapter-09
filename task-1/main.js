ea247076bc5fc50016cf7eaba65cb9a2e8b60bf0
unknown <iwork.mail@mail.ru>
2019-05-19 16:16:18 +0300

    alpha 0.1.0

--- task-1/main.js
+++ task-1/main.js
@@ -1,22 +1,38 @@
-/* const markdownToHTMLInput = document.getElementById('markdownToHTMLInput');
+const markdownToHTMLInput = document.getElementById('markdownToHTMLInput');
 const markdownToHTMLResult = document.getElementById('markdownToHTMLResult');
 // const input = '**text**';
-function markdownToHTML() {
-  let text = markdownToHTMLInput.nodeValue;
-  // input.replace();
-  text = text.replace(/#.+/, '<h1>$&</h1>');
-  // text = text.replace(/<h1>#/, '<h1>');
-  // let result = input.replace(/\*{2}.+\*{2}/, '<b>$&</b>');
-  markdownToHTMLResult.innerHTML = text;
-}
+// function markdownToHTML() {
+//   let text = markdownToHTMLInput.value;
+//   // input.replace();
+//   text = text.replace(/#.+/, '<h1>$&</h1>');
+//   text = text.replace(/<h1>#/, '<h1>');
+//   // let result = input.replace(/\*{2}.+\*{2}/, '<b>$&</b>');
+//   markdownToHTMLResult.innerHTML = text;
+// }
 // markdownToHTML();
-console.log(markdownToHTML()); */
+// console.log(markdownToHTML());
 
-const input = '**text**';
+// const input = '**text**';
+// function markdownToHTML() {
+//   input.replace();
+//   let result = input.replace(/\*{2}.+\*{2}/, '<b>');
+//   return result;
+// }
+// // markdownToHTML();
+// console.log(markdownToHTML());
+
 function markdownToHTML() {
-  input.replace();
-  let result = input.replace(/\*{2}.+\*{2}/, '<b>');
-  return result;
+  const value = markdownToHTMLInput.value;
+  const lines = value.split('\n');
+
+  const result = lines.map(line => {
+    const res = line
+      .replace(/^#(.*)/, '<h1>$1</h1>')
+      .replace(/\*{2}(\w+)\*{2}/g, '<b>$1</b>');
+    return res;
+  });
+
+  markdownToHTMLResult.innerHTML = result.join('\n');
 }
-// markdownToHTML();
-console.log(markdownToHTML());
+
+markdownToHTMLInput.addEventListener('keyup', markdownToHTML);
