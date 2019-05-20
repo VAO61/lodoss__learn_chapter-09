f8f7619e09a5816d3faf6ddaed603150404b7a7b
VAO61 <vao.frontend@gmail.com>
2019-05-20 14:46:48 +0300

    1.0.0

--- task-1/main.js
+++ task-1/main.js
@@ -1,25 +1,5 @@
 const markdownToHTMLInput = document.getElementById('markdownToHTMLInput');
 const markdownToHTMLResult = document.getElementById('markdownToHTMLResult');
-// const input = '**text**';
-// function markdownToHTML() {
-//   let text = markdownToHTMLInput.value;
-//   // input.replace();
-//   text = text.replace(/#.+/, '<h1>$&</h1>');
-//   text = text.replace(/<h1>#/, '<h1>');
-//   // let result = input.replace(/\*{2}.+\*{2}/, '<b>$&</b>');
-//   markdownToHTMLResult.innerHTML = text;
-// }
-// markdownToHTML();
-// console.log(markdownToHTML());
-
-// const input = '**text**';
-// function markdownToHTML() {
-//   input.replace();
-//   let result = input.replace(/\*{2}.+\*{2}/, '<b>');
-//   return result;
-// }
-// // markdownToHTML();
-// console.log(markdownToHTML());
 
 function markdownToHTML() {
   const value = markdownToHTMLInput.value;
@@ -27,6 +7,11 @@ function markdownToHTML() {
 
   const result = lines.map(line => {
     const res = line
+      // .replace(/^#{6}(.*)/, '<h6>h6</h6>')
+      // .replace(/^#{5}(.*)/, '<h5>h5</h5>')
+      // .replace(/^#{4}(.*)/, '<h4>h4</h4>')
+      // .replace(/^#{3}(.*)/, '<h3>h3</h3>')
+      // .replace(/^#{2}(.*)/, '<h2>h2</h2>')
       .replace(/^#(.*)/, '<h1>$1</h1>')
       .replace(/\*{2}(\w+)\*{2}/g, '<b>$1</b>');
     return res;
