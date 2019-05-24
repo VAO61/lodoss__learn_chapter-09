b47ff830789f040b220f4057cad7de1f87c57fea
VAO61 <vao.frontend@gmail.com>
2019-05-24 18:33:42 +0300

    Webpack 1st stable bundler | UPD: webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,49 @@
 const path = require('path');
-// const HTMLWebpackPlugin = require('html-webpack-plugin');
-
-const PATHS = {
-  source: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'dist')
-};
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: PATHS.source + '/index.js',
+  entry: './src/index.js',
   output: {
-    path: PATHS.build,
-    filename: '[name].js'
-  }
-  // plugins: [
-  //   new HTMLWebpackPlugin({
-  //     title: 'Webpack app'
-  //   })
-  // ]
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main-[hash].js'
+  },
+  devServer: {
+    // hot: true,
+    // inline: true,
+    // open: true,
+    // port: 3000,
+    contentBase: './src'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style-[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'task-1.html',
+      template: './src/html/task-1.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'task-2.html',
+      template: './src/html/task-2.html'
+    })
+  ]
 };
