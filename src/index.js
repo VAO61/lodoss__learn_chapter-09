b4663f9cd32d6a34eeb39fcdf53d89ac6e93ce51
VAO61 <vao.frontend@gmail.com>
2019-06-03 18:06:33 +0300

    UPD: file-loader config url's

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // import './style.css';
 import './style.scss';
 
+if (process.env.NODE_ENV !== 'production') {
+  require('file-loader!./html/task-1.html');
+  require('file-loader!./html/task-2.html');
+}
+
 import './js/markdown-to-html';
 import './js/rendering';
 import './js/validation';
-
-if (process.env.NODE_ENV !== 'production') {
-  require('file-loader!./index.html');
-  // require('file-loader!./html/task-2.html');
-}
