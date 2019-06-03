1da1d49bac6e80c57c650a99208e3889b507feca
VAO61 <vao.frontend@gmail.com>
2019-06-03 15:08:40 +0300

    UPD: config

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import './style.scss';
 import './js/markdown-to-html';
 import './js/rendering';
 import './js/validation';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('file-loader!./index.html');
+  // require('file-loader!./html/task-2.html');
+}
